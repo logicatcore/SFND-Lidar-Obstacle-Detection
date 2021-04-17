78b5cd756bb0aae05cfbd20e039081f7c7d4ae91
sai <kakubalsai@gmail.com>
2021-04-17 13:22:26 +0200

    KD-tree search implementation

--- src/quiz/cluster/kdtree.h
+++ src/quiz/cluster/kdtree.h
@@ -58,14 +58,34 @@ struct KdTree
 		place(&root, point, id, 0);
 	}
 
+	void digThrough(Node **n, std::vector<float> *target, std::vector<int> *ids, float *tol, uint depth)
+	{
+		uint cd = depth % 2;
+		if(*n != NULL){
+			if(abs((*target)[0] - (*n)->point[0]) <= *tol & abs((*target)[1] - (*n)->point[1]) <= *tol)
+			{
+				float dist = sqrt(pow((*target)[0] - (*n)->point[0], 2) + pow((*target)[1] - (*n)->point[1], 2));
+				if (dist < *tol)
+					(*ids).push_back((*n)->id);
+			}
+			
+			if(((*target)[cd] - *tol) < (*n)->point[cd])
+			{
+				digThrough(&((*n)->left), target, ids, tol, ++depth);
+			}
+			if(((*target)[cd] + *tol) > (*n)->point[cd])
+			{
+				digThrough(&((*n)->right), target, ids, tol, ++depth);
+			}
+		}
+	}
 	// return a list of point ids in the tree that are within distance of target
 	std::vector<int> search(std::vector<float> target, float distanceTol)
 	{
 		std::vector<int> ids;
+		digThrough(&root, &target, &ids, &distanceTol, 0);
 		return ids;
 	}
-	
-
 };
 
 
