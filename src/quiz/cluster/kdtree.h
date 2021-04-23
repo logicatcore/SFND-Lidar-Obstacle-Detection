4cf54e530103e519ffd3ffec10ece178a7dd7413
sai <kakubalsai@gmail.com>
2021-04-23 10:57:27 +0200

    Overload functions to handle PCL point types

--- src/quiz/cluster/kdtree.h
+++ src/quiz/cluster/kdtree.h
@@ -8,7 +8,7 @@
 template <typename PointT>
 struct Node
 {
-	std::vector<PointT> point;
+	PointT point;
 	int id;
 	Node<PointT>* left;
 	Node<PointT>* right;
@@ -57,6 +57,48 @@ struct KdTree
 			place(&((*n)->right), point, id, ++depth);
 		}
 	}
+	// overload to handle pcl point types, can handle PointXY and PointXYZ
+	void place(Node<PointT> **n, PointT &point, int &id, uint depth) 
+	{
+		if(*n == NULL)
+		{
+			*n = new Node<PointT>(point, id);
+		}
+		else {
+			switch (depth % D){
+				case 0:
+								if(point.x < (*n)->point.x)
+								{
+									place(&((*n)->left), point, id, ++depth);
+								}
+								else
+								{
+									place(&((*n)->right), point, id, ++depth);
+								}
+								break;
+				case 1:
+								if(point.y < (*n)->point.y)
+								{
+									place(&((*n)->left), point, id, ++depth);
+								}
+								else
+								{
+									place(&((*n)->right), point, id, ++depth);
+								}
+								break;
+				case 2:
+								if(point.z < (*n)->point.z)
+								{
+									place(&((*n)->left), point, id, ++depth);
+								}
+								else
+								{
+									place(&((*n)->right), point, id, ++depth);
+								}
+								break;
+			}
+		}
+	}
 
 	void insert(std::vector<PointT> point, int id)
 	{
@@ -64,6 +106,7 @@ struct KdTree
 		place(&root, point, id, 0);
 	}
 
+  // overload to handle pcl point types, can handle PointXY and PointXYZ
 	void insert(PointT point, int id)
 	{
 		// the function should create a new node and place correctly with in the root 
@@ -92,6 +135,52 @@ struct KdTree
 		}
 	}
 
+  // overload to handle pcl point types, can handle PointXY and PointXYZ
+	void digThrough(Node<PointT> **n, PointT *target, std::vector<int> *ids, float *tol, uint depth)
+	{
+		if(*n != NULL){
+			if(abs((*target).x - (*n)->point.x) <= *tol & abs((*target).y - (*n)->point.y) <= *tol & abs((*target).z - (*n)->point.z) <= *tol)
+			{
+				float dist = sqrt(pow((*target).x - (*n)->point.x, 2) + pow((*target).y - (*n)->point.y, 2) + pow((*target).z - (*n)->point.z, 2));
+				if (dist < *tol)
+					(*ids).push_back((*n)->id);
+			}
+
+			switch (depth % D){
+				case 0:
+							if(((*target).x - *tol) < (*n)->point.x)
+							{
+								digThrough(&((*n)->left), target, ids, tol, ++depth);
+							}
+							if(((*target).x + *tol) > (*n)->point.x)
+							{
+								digThrough(&((*n)->right), target, ids, tol, ++depth);
+							}
+							break;
+				case 1:
+							if(((*target).y - *tol) < (*n)->point.y)
+							{
+								digThrough(&((*n)->left), target, ids, tol, ++depth);
+							}
+							if(((*target).y + *tol) > (*n)->point.y)
+							{
+								digThrough(&((*n)->right), target, ids, tol, ++depth);
+							}
+							break;
+				case 2:
+							if(((*target).z - *tol) < (*n)->point.z)
+							{
+								digThrough(&((*n)->left), target, ids, tol, ++depth);
+							}
+							if(((*target).z + *tol) > (*n)->point.z)
+							{
+								digThrough(&((*n)->right), target, ids, tol, ++depth);
+							}
+							break;
+			}
+		}
+	}
+
 	// return a list of point ids in the tree that are within distance of target
 	std::vector<int> search(std::vector<PointT> target, float distanceTol)
 	{
@@ -100,6 +189,7 @@ struct KdTree
 		return ids;
 	}
 
+	// overload to handle pcl point types
 	// return a list of point ids in the tree that are within distance of target
 	std::vector<int> search(PointT target, float distanceTol)
 	{
